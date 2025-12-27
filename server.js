6cd328172bfb95721b8fd7dbb8ddc798ba9c5989
ui-yashaswi <itsgeekyyashaswi@gmail.com>
2025-12-27 20:42:43 +0530

    'fjdfkj'

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-import authRoutes from './routes/authRoutes.js';
-import topicRoutes from './routes/topicRoutes.js';
-import progressRoutes from './routes/progressRoutes.js';
+import authRoutes from "./routes/authRoutes.js";
+import topicRoutes from "./routes/topicRoutes.js";
+import progressRoutes from "./routes/progressRoutes.js";
 
 dotenv.config();
 
@@ -14,28 +14,35 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-const allowedOrigins = ['http://localhost:5173', 'https://dsa-sheet-frontend-virid.vercel.app'];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://dsa-sheet-frontend-virid.vercel.app",
+  "https://dsa-backend-qqet.onrender.com",
+];
 
-app.use(cors({
+app.use(
+  cors({
     origin: allowedOrigins,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/topics', topicRoutes);
-app.use('/api/progress', progressRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/topics", topicRoutes);
+app.use("/api/progress", progressRoutes);
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
